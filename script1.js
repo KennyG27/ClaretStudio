d32321aa4b638da69571ba35c7a8ef60b038fd8c
KennyG27 <143297241+KennyG27@users.noreply.github.com>
2024-02-08 01:04:44 -0500

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,23 @@
+const registrationForm = document.getElementById('registrationForm');
+const usernameInput = document.getElementById('username');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const messageElement = document.getElementById('message');
+
+// Base de datos falsa en memoria
+const users = [];
+
+registrationForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const newUser = {
+    username: usernameInput.value,
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+  users.push(newUser);
+  messageElement.textContent = 'Registro Existoso Bienvenido Claretiano!';
+  setTimeout(() => {
+    messageElement.textContent = '';
+    window.location.href = 'index2.html';
+  }, 3000);
+});
